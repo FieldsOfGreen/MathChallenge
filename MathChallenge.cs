a9e915405753e6bc1d7c1ca86c6672594eeafee2
FieldsOfGreen <jrosales0001@gmail.com>
2018-01-08 16:36:03 -0500

    spacing changes

--- MathChallenge.cs
+++ MathChallenge.cs
@@ -4,12 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 namespace MathChallenge
 {
-
-
     class Program
     {
         public static void Main(string[] args)
@@ -17,17 +13,13 @@ namespace MathChallenge
             bool runprogram = true;
             char doagain;
 
-
-
             while (runprogram)
             {
-
                 Console.WriteLine("Please enter a number");
                 var input1 = Console.ReadLine();
                 int input1digits = input1.ToString().Length;
 
                 Console.WriteLine("Please enter a second number with the same number of digits");
-                //Write: how to make it so user enters a number with the SAME NUMBER OF DIGITS as the first number they enter.
                 var input2 = Console.ReadLine();
                 int input2digits = input2.ToString().Length;
 
@@ -35,14 +27,11 @@ namespace MathChallenge
                 {
                     Console.WriteLine("Please try again. Make sure each number has the same amount of digits.");
                 }
-                    
-                                
+
                 //validates that user entered integers
                 if (int.TryParse(input1 + input2, out int number1))
                 {
                     Console.WriteLine("Your numbers are: {0} and {1}", input1, input2);
-
-
                 }
                 else
                 {
@@ -53,7 +42,6 @@ namespace MathChallenge
                 Console.WriteLine("Would you like to run this program again? (Y or N)");
                 //Error: if user enters anything but a single character. 
                 doagain = Convert.ToChar(Console.ReadLine());
-
                 if (doagain != 'y' && doagain != 'Y')
                 {
                     runprogram = false;
